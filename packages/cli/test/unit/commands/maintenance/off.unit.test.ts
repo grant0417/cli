2bb0a5a5d751a7a05df77a4c3c2d48721d27d95b
Zane Whitfield <zwhitfield@salesforce.com>
2023-11-15 16:25:27 -0800

    WIP migrate command & tests

--- packages/cli/test/unit/commands/maintenance/off.unit.test.ts
+++ packages/cli/test/unit/commands/maintenance/off.unit.test.ts
@@ -1,16 +1,29 @@
 import {expect, test} from '@oclif/test'
 
-describe('maintenance:off', () => {
+describe('maintenance', () => {
   test
     .stdout()
     .stderr()
     .nock('https://api.heroku.com:443', api => api
-      .patch('/apps/myapp', {maintenance: false})
-      .reply(200),
+      .get('/apps/myapp')
+      .reply(200, {maintenance: true}),
     )
-    .command(['maintenance:off', '-a', 'myapp'])
-    .it('turns maintenance mode off', ({stdout, stderr}) => {
-      expect(stdout).to.be.empty
-      expect(stderr).to.contain('Disabling maintenance mode for ⬢ myapp... done')
+    .command(['maintenance', '-a', 'myapp'])
+    .it('shows that maintenance is on', ({stdout, stderr}) => {
+      expect(stdout).to.equal('on\n')
+      expect(stderr).to.be.empty
+    })
+
+  test
+    .stdout()
+    .stderr()
+    .nock('https://api.heroku.com:443', api => api
+      .get('/apps/myapp')
+      .reply(200, {maintenance: false}),
+    )
+    .command(['maintenance', '-a', 'myapp'])
+    .it('shows that maintenance is off', ({stdout, stderr}) => {
+      expect(stdout).to.equal('off\n')
+      expect(stderr).to.be.empty
     })
 })
